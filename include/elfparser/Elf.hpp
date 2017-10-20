b42a344aca1e6ea546374605a7ad90949e428673
Domonkos Suranyi <suranyi.domi@gmail.com>
2017-10-20 18:16:12 +0200

    Elf::readFile pvt function added

--- include/elfparser/Elf.hpp
+++ include/elfparser/Elf.hpp
@@ -1,13 +1,23 @@
 #ifndef ELFPARSER_ELF_HPP_INC
 #define ELFPARSER_ELF_HPP_INC
 
+#include <string>
+#include <vector>
+
 namespace elf
 {
-  class Elf
-  {
-    public:
-      Elf();
-      ~Elf();
-  };
+    typedef unsigned char byte;
+
+    class Elf
+    {
+      public:
+        std::vector<byte> m_elfBinary;
+
+        Elf(std::string);
+        ~Elf();
+
+      private:
+        std::vector<byte> readFile(std::string);
+    };
 }
 #endif
--- src/Elf.cpp
+++ src/Elf.cpp
@@ -1,10 +1,13 @@
 #include <elfparser/Elf.hpp>
 
+#include <fstream>
+#include <iterator>
+
 using namespace elf;
 
-Elf::Elf()
+Elf::Elf(std::string elfFilePath)
 {
-
+    m_elfBinary = readFile(elfFilePath);
 }
 
 Elf::~Elf()
@@ -12,3 +15,21 @@ Elf::~Elf()
 
 }
 
+std::vector<byte> Elf::readFile(std::string path)
+{
+    std::ifstream file(path, std::ios::binary);
+    file.unsetf(std::ios::skipws);
+
+    std::streampos fileSize;
+    file.seekg(0, std::ios::end);
+    fileSize = file.tellg();
+    file.seekg(0, std::ios::beg);
+
+    std::vector<byte> retVector;
+    retVector.reserve(fileSize);
+
+    retVector.insert(retVector.begin(),
+                     std::istream_iterator<byte>(file),
+                     std::istream_iterator<byte>());
+    return retVector;
+}
