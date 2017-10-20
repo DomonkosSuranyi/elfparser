139c226f77caa196a3d31711016dfd757829ad23
Domonkos Suranyi <suranyi.domi@gmail.com>
2017-10-20 14:52:52 +0200

    Elf.cpp added with header

---
+++ include/elfparser/Elf.hpp
@@ -0,0 +1,13 @@
+#ifndef ELFPARSER_ELF_HPP_INC
+#define ELFPARSER_ELF_HPP_INC
+
+namespace elf
+{
+  class Elf
+  {
+    public:
+      Elf();
+      ~Elf();
+  }
+}
+#endif
---
+++ src/Elf.cpp
@@ -0,0 +1,14 @@
+#include <elfparser/Elf.hpp>
+
+using namespace elf;
+
+Elf::Elf()
+{
+
+}
+
+Elf::~Elf()
+{
+
+}
+
